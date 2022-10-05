213d4c0ac3619d336c94035f5803517b83d6d2b6
David Giesemann <info@david-giesemann.de>
2022-10-05 17:59:07 +0200

    styling github page

--- examples/index.js
+++ examples/index.js
@@ -11,13 +11,26 @@ document.querySelectorAll(".default").forEach(el => {
 })
 
 document.querySelectorAll(".step-25").forEach(el => {
-    new RangeSlider(el,{
+    new RangeSlider(el, {
         outputFormatter: o => `${o.toFixed(2)} kg`
     })
 })
 
 document.querySelectorAll(".step-lg").forEach(el => {
-    new RangeSlider(el,{
-        outputFormatter: o => `${o/1000} k`
+    new RangeSlider(el, {
+        outputFormatter: o => `${o / 1000} k`
     })
 })
+
+document.querySelectorAll(".no-output").forEach(el => {
+    const slider = new RangeSlider(el, {
+        output: false
+    })
+
+    const button = document.createElement("button")
+    button.textContent = "Alert slider values"
+    button.onclick = () => alert(`Your values are: ${slider.min} and ${slider.max}`)
+
+    el.after(button)
+})
+
